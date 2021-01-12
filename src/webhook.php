a83556714fb76040ce0dc81b7021111efb1bc7c7
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-01-12 02:09:56 +0100

    FlexiBee renamed to AbraFlexi Current dependencies used

---
+++ src/webhook.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AbraFlexi\Acceptor;
+
+/**
+ * System.Spoje.Net - WebHook Acceptor & Saver to SQL Cache.
+ *
+ * @author     Vítězslav Dvořák <vitex@arachne.cz>
+ * @copyright  2017-2020 Spoje.Net
+ */
+define('APP_NAME', 'WebHookAcceptor');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$hooker = new \AbraFlexi\Bricks\HookReciever();
+//$hooker->debug = true;
+
+$apiResponseRaw = $hooker->listen();
+if (!empty($apiResponseRaw) && $hooker->takeApiChanges($apiResponseRaw)) {
+    $hooker->saveWebhookData($hooker->onlyFreshHooks($hooker->changes));
+}
