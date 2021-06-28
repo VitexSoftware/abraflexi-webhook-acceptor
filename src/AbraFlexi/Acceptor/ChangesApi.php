7739fcbd22827c5ce402262a54a0303960bf382b
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-28 06:30:37 +0200

    Current state to build

---
+++ src/AbraFlexi/Acceptor/ChangesApi.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AbraFlexi\Acceptor;
+
+/**
+ * Description of Api
+ *
+ * @author vitex
+ */
+class ChangesApi extends \Ease\SQL\Engine {
+
+    public $myTable = 'changesapi';
+
+}
