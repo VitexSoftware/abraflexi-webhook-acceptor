7e99c1ac3abdfcd2a562c95afea8c8b02d677a51
Vítězslav Dvořák <info@vitexsoftware.cz>
2022-09-06 22:36:06 +0200

    Improved landingpage

---
+++ src/webhook.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AbraFlexi\Acceptor;
+
+/**
+ * System.Spoje.Net - WebHook Acceptor & Saver to SQL Cache.
+ *
+ * @author     Vítězslav Dvořák <vitex@arachne.cz>
+ * @copyright  2017-2020 Spoje.Net, 2021-2022 VitexSoftware
+ */
+define('APP_NAME', 'WebHookAcceptor');
+define('EASE_LOGGER', 'syslog');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$cfg = '../.env';
+if (file_exists($cfg)) {
+    \Ease\Shared::singleton()->loadConfig($cfg, true);
+}
+if (\Ease\WebPage::isPosted()) {
+    try {
+        $hooker = new HookReciever();
+        $hooker->debug = true;
+        $apiResponseRaw = $hooker->listen();
+        if (!empty($apiResponseRaw) && $hooker->takeChanges($apiResponseRaw)) {
+            $hooker->saveWebhookData($hooker->onlyFreshHooks($hooker->changes));
+        } else {
+            echo 'No changes';
+        }
+    } catch (\AbraFlexi\Exception $exc) {
+        echo $exc->getMessage();
+        exit(500);
+    }
+} else {
+    $oPage = new \Ease\TWB4\WebPage(\Ease\Shared::appName());
+    $oPage->redirect('installer.php');
+    $oPage->addItem(new \Ease\Html\ATag('installer.php', _('Run the installer')));
+    echo $oPage;
+}
